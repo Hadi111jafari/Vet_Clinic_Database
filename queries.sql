95cbf8089229353cb04efc7e1fc57d78c966ea3f
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-06-09 11:56:52 +0430

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -9,3 +9,53 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name!='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+BEGIN TRANSACTION;
+SAVEPOINT SP1;
+ROLLBACK TO SP1;
+ROLLBACK;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon%';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth BETWEEN '2022-1-1' AND '2050-1-1';
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals;
+SELECT COUNT(neutered) FROM animals WHERE neutered = false;
+SELECT COUNT(neutered) FROM animals WHERE neutered = true;
+
+SELECT species, MIN(weight_kg) 
+FROM animals 
+GROUP BY species;
+
+SELECT species, MAX(weight_kg) 
+FROM animals 
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) 
+FROM animals WHERE date_of_birth BETWEEN '1990-1-1' AND '2000-1-1'
+GROUP BY species;
